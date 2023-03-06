471cdfd9af2729dea45408e2c967457a2f1a32ad
Renee Schmidt <RJS2018@gmail.com>
2023-03-06 13:49:52 -0500

    Hello world api placeholder

--- api-interface.py
+++ api-interface.py
@@ -3,5 +3,16 @@
 Created on Mon Mar  6 13:35:26 2023
 
 @author: RENEEEEEEE
+
+To Run: uvicorn api-interface:app --reload
 """
 
+# imports
+from fastapi import FastAPI
+
+# init app
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
