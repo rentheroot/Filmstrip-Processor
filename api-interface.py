a471c1dcea28831af50f56a3bbb54b0294c866b6
Renee Schmidt <RJS2018@gmail.com>
2023-03-06 14:25:16 -0500

    define subdivision request body

--- api-interface.py
+++ api-interface.py
@@ -9,9 +9,17 @@ To Run: uvicorn api-interface:app --reload
 
 # imports
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # init app
 app = FastAPI()
+
+# Define image subsection / mask area
+# formatted for cv2.rectangle
+class Subdivision(BaseModel):
+    id: int
+    start_point: tuple[int] = []
+    end_point: tuple[int] = []
 
 @app.get("/")
 async def root():
